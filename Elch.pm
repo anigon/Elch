2d31cde7fd8e6dd1f12e4f267dec386b9e6b880d
anigon <anigon@localhost.localdomain>
2010-05-01 20:07:42 +0900

    bug fix

--- Elch.pm
+++ Elch.pm
@@ -1,28 +1,70 @@
 package Elch;
 
-use Exporter;
-@ISA    = (Exporter);
-@EXPORT = qw(has extends);
+use strict;
+use warnings;
+use Carp;
 
-use Data::Dumper;
-my ($class, $file, $line)  = caller;
+use constant PROTECTED => 10;
+use constant PRIVATE   => 20;
 
-#no strict 'refs';
+my $new = sub {
+    my $self    = shift;
+    my (%arg)   = @_;
 
-sub extends {
+    my $member  = {};
+    foreach my $key (keys %arg) {
+        $member->{$key} = $arg{$key};
+    }
+
+    bless $member, $self;
+};
+
+my $extends = sub {
     my $super_class = shift;
+    my ($pkg)       = caller;
     eval {
-#CORE::require("$super_class");
-        "use $super_class";
+        use Module::Load;
+        load $super_class;
+        no strict 'refs';
+        @{"${pkg}::ISA"} = $super_class;
     };
     if ($@) {
-        die ("can't load super class $super_class $@");
+        confess "can't load super class $super_class $@";
+    }
+};
+
+sub _can_execute {
+    my ($pkg, $method, $caller_class_name) = @_;
+
+    return unless $method =~ /^_(_?)/;
+
+    my $scope = ($1 ? PRIVATE : PROTECTED);
+
+    if ($scope == PRIVATE) {
+        $caller_class_name eq $pkg
+            or confess "$method cannot be called from other class";
+
+    } elsif ($scope == PROTECTED) {
+        return if $caller_class_name eq $pkg; # itself
+
+        my $is_subclass = 0;
+
+        no strict 'refs';
+        for (@{"${caller_class_name}::ISA"}) {
+            next unless $_ =~ /^$pkg$/;
+            $is_subclass++; last;
+        }
+        use strict 'refs';
+
+        $is_subclass
+            or confess "$method cannot be called from other package";
     }
 }
 
-sub has {
+my $has = sub {
     my $method  = shift;
     my ($arg)   = @_;
+    my ($pkg)   = caller;
 
     my $default;
     $default    = $arg->{'default'} if exists $arg->{'default'};
@@ -30,16 +72,37 @@ sub has {
     my $routine = sub {
 
         # check scope
-        if ($method =~ /^_/) { # private method
-            my ($caller_class_name) = caller();
-            $caller_class_name eq $class
-                or die "this method cannot be called from other class";
-        }
+        my ($caller_class_name) = caller();
+        &_can_execute($pkg, $method, $caller_class_name);
+#        if ($method =~ /^_(_?)/) {
+#            my $is_private          = $1;
+#            my ($caller_class_name) = caller();
+#
+#            if ($is_private) {
+#                $caller_class_name eq $pkg
+#                    or confess "$method cannot be called from other class";
+#
+#            } else { # protected method
+#                if ($caller_class_name ne $pkg) {
+#                    my $is_subclass = 0;
+#
+#                    no strict 'refs';
+#                    for (@{"${caller_class_name}::ISA"}) {
+#                        next unless $_ =~ /^$pkg$/;
+#                        $is_subclass++; last;
+#                    }
+#                    use strict 'refs';
+#
+#                    $is_subclass
+#                        or confess "$method cannot be called from other package";
+#                }
+#            }
+#        }
 
         # check arg
         if (scalar @_ > 1) {
-            if ($is eq 'ro') { # read only
-                die "this is readonly";
+            if ($arg->{is} eq 'ro') { # read only
+                confess "this is readonly";
             } else { # read write (* default)
                 $_[0]->{$method} = $_[1];
             }
@@ -55,19 +118,18 @@ sub has {
         return $_[0]->{$method};
     };
 
-    *{"${class}::${method}"}   = $routine;
-}
-
-*{"${class}::new"} = sub {
-    my $self    = shift;
-    my (%arg)   = @_;
-
-    my $member  = {};
-    foreach my $key (keys %arg) {
-        $member->{$key} = $arg{$key};
-    }
-
-    bless $member, $self;
+    no strict 'refs';
+    *{"${pkg}::${method}"}   = $routine;
 };
 
-1;
+sub import {
+    my $class = shift;
+    my ($pkg) = caller;
+
+    no strict 'refs';
+    *{"${pkg}::new"}     = $new;
+    *{"${pkg}::has"}     = $has;
+    *{"${pkg}::extends"} = $extends;
+}
+
+1; # end of this class
